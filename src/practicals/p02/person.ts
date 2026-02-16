a0b1cbf3cfbc0020c2b9832efb9b05ed65d46ebe
Natthawut Putthong <68010375@kmitl.ac.th>
2026-02-16 10:07:00 +0700

    k

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,5 +1,23 @@
 export class Person {
-    firstname:string
-    lastname:string
+    firstname?:string
+    lastname?:string
+    private age? = 0;
+    static COUNTRY:string = "thailand"
 
+    constructor(firstname:string,lastname:string){
+        if(firstname){
+            this.firstname=firstname
+        }
+        if(lastname){
+            this.lastname=lastname
+        }
+        this.lastname=lastname
+    }setAge(value:number){
+        this.age = value; 
+    }getAge(){
+        return this.age; 
+
+    }getFullName(){
+
+    }
 }
