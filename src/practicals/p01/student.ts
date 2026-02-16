084d788138857c5d01e22c69baff0632cf6fb4bb
Natthawut Putthong <68010375@kmitl.ac.th>
2026-02-16 09:34:42 +0700

    b

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -2,7 +2,7 @@ export class Student {
     firstname?:string
     lastname?:string
 
-constructor(firstname:string,lastname:string){
+constructor(firstname?:string,lastname?:string){
     this.firstname=firstname
     this.lastname=lastname
 
